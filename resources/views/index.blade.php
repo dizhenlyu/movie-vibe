5afb730a3c0173db2cb0e579736c7c2718d2dd57
Dizhen Lu <dizhenlu@usc.edu>
2015-04-28 23:08:23 -0700

    Adjust video size

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,7 +31,7 @@
         $(function() {
             BV = new $.BigVideo();
             BV.init();
-            BV.show('http://diworkstation.com/mrnailart/wp-content/uploads/2015/04/movies.webm',{ambient:true});
+            BV.show('http://diworkstation.com/mrnailart/wp-content/uploads/2015/04/movies-short.mp4',{ambient:true});
         });
     </script>
 @stop
