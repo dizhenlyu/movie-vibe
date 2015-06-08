1bbcb149f4cb5cfe0ee7ebb54baf88b35b66ff32
Dizhen Lu <dizhenlu@usc.edu>
2015-06-08 12:35:01 -0700

    Fix small bugs for index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,11 +18,11 @@
 
 @section('scripts')
     <!-- BigVideo Dependencies -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/jquery-1.8.1.min.js"><\/script>')</script>
     <script src="js/jquery-ui.min.js"></script>
     <script src="js/jquery.imagesloaded.min.js"></script>
-    <script src="http://vjs.zencdn.net/c/video.js"></script>
+    <script src="https://vjs.zencdn.net/c/video.js"></script>
 
     <!-- BigVideo -->
     <script src="js/bigvideo.js"></script>
